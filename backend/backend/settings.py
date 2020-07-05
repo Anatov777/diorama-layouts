1006a6bafe4b1797a5edffc6b30de6836cfbf865
Admin <anatovtimur@gmail.com>
2020-07-05 09:54:29 -0700

    make it better

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'xm(t^kq(^mtsh+rw&yqbxmgh3s^fx=nz4y0m04#fyoyc1)ha*8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
